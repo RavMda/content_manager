45f71c2026416c9a8ca25e7395fce507b836afa4
RavMda <49620461+RavMda@users.noreply.github.com>
2023-11-14 14:25:56 +0200

    remove useless macro

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
+use std::{
+	error::Error,
+	fs,
+	path::{Path, PathBuf},
+};
+
 mod parser;
 mod tests;
 
-fn main() {
-	let file = std::fs::read("./src/tests/models/keycard.mdl").expect("error opening models/fence.mdl");
-	let parsed = match parser::parse_model(&file) {
-		Ok(parsed) => parsed,
-		Err(err) => panic!("{}", err),
-	};
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
-	println!(
-		"directories: {:?}\ntextures: {:?}",
-		parsed.directories, parsed.textures
-	);
+fn main() -> Result<()> {
+	let input_path = Path::new("input");
+	let output_path = Path::new("output");
+
+	let addon_packs: Vec<PathBuf> = fs::read_dir(input_path)?
+		.into_iter()
+		.filter_map(|entry| entry.ok())
+		.map(|entry| entry.path())
+		.filter(|entry| entry.is_dir())
+		.collect();
+
+	// create same folders in ./output
+
+	if output_path.exists() {
+		fs::remove_dir_all(output_path)?;
+	}
+
+	fs::create_dir(output_path)?;
+
+	for entry in addon_packs.iter() {
+		let entry_name = entry
+			.file_name()
+			.ok_or("failed to get file name")?
+			.to_str()
+			.ok_or("failed to convert file name to string")?;
+
+		fs::create_dir(output_path.join(entry_name))?;
+	}
+
+	// create _lua additionally
+	fs::create_dir(output_path.join("_lua"))?;
+
+	Ok(())
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -24,25 +24,11 @@ macro_rules! safe_return {
 	};
 }
 
-macro_rules! unwrap_or_return {
-	($result:expr) => {
-		match $result {
-			Ok(value) => value,
-			Err(err) => {
-				return Err(
-					Box::new(io::Error::new(io::ErrorKind::Other, err.to_string()))
-						as Box<dyn std::error::Error + Send + Sync>,
-				)
-			}
-		}
-	};
-}
-
 impl<'a> ModelReader<'a> {
 	fn read_string(&mut self, size: usize) -> Result<String> {
 		let mut string_vec = vec![0u8; size];
 
-		unwrap_or_return!(self.reader.read_exact(&mut string_vec));
+		self.reader.read_exact(&mut string_vec)?;
 
 		safe_return!(String::from_utf8(string_vec));
 	}
@@ -53,9 +39,9 @@ impl<'a> ModelReader<'a> {
 
 	#[allow(dead_code)]
 	fn read_vector(&mut self) -> Result<Vec<f32>> {
-		let x = unwrap_or_return!(self.reader.read_f32::<LittleEndian>());
-		let y = unwrap_or_return!(self.reader.read_f32::<LittleEndian>());
-		let z = unwrap_or_return!(self.reader.read_f32::<LittleEndian>());
+		let x = self.reader.read_f32::<LittleEndian>()?;
+		let y = self.reader.read_f32::<LittleEndian>()?;
+		let z = self.reader.read_f32::<LittleEndian>()?;
 
 		return Ok(vec![x, y, z]);
 	}
@@ -63,7 +49,7 @@ impl<'a> ModelReader<'a> {
 	fn read_byte(&mut self) -> Result<u8> {
 		let mut byte_buf: [u8; 1] = [0];
 
-		unwrap_or_return!(self.reader.read(&mut byte_buf));
+		self.reader.read(&mut byte_buf)?;
 
 		return Ok(byte_buf[0]);
 	}
@@ -72,7 +58,7 @@ impl<'a> ModelReader<'a> {
 		let mut string_vec: Vec<u8> = vec![];
 
 		loop {
-			let byte = unwrap_or_return!(self.read_byte());
+			let byte = self.read_byte()?;
 
 			if byte == 0 {
 				break;
@@ -114,7 +100,7 @@ pub fn parse_model(file: &Vec<u8>) -> Result<ParsedModel> {
 		reader: std::io::Cursor::new(&file),
 	};
 
-	let model_format = unwrap_or_return!(model_reader.read_string(4));
+	let model_format = model_reader.read_string(4)?;
 
 	if model_format != "IDST" {
 		return Err(Box::new(ErrorModelFormat(model_format)));
@@ -122,28 +108,28 @@ pub fn parse_model(file: &Vec<u8>) -> Result<ParsedModel> {
 
 	model_reader.skip(200);
 
-	let texture_count = unwrap_or_return!(model_reader.read_int());
-	let texture_offset = unwrap_or_return!(model_reader.read_int());
-	let texturedir_count = unwrap_or_return!(model_reader.read_int());
-	let texturedir_offset = unwrap_or_return!(model_reader.read_int());
+	let texture_count = model_reader.read_int()?;
+	let texture_offset = model_reader.read_int()?;
+	let texturedir_count = model_reader.read_int()?;
+	let texturedir_offset = model_reader.read_int()?;
 
 	model_reader.set_pos(texturedir_offset);
 
-	let texturedir_string_offset = unwrap_or_return!(model_reader.read_int());
+	let texturedir_string_offset = model_reader.read_int()?;
 	model_reader.set_pos(texturedir_string_offset);
 
 	for _ in 0..texturedir_count {
-		let dir = unwrap_or_return!(model_reader.read_c_str());
+		let dir = model_reader.read_c_str()?;
 		model_textures.directories.push(dir);
 	}
 
 	model_reader.set_pos(texture_offset);
 
-	let texture_path_offset = unwrap_or_return!(model_reader.read_int()) + texture_offset;
+	let texture_path_offset = model_reader.read_int()? + texture_offset;
 	model_reader.set_pos(texture_path_offset);
 
 	for _ in 0..texture_count {
-		let texture = unwrap_or_return!(model_reader.read_c_str());
+		let texture = model_reader.read_c_str()?;
 		model_textures.textures.push(texture);
 	}
 
